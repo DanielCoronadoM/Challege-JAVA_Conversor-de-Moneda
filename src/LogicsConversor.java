956fd9e6469d04cac36dc506a1a71aed158d645f
Daniel_C <danhc7417@gmail.com>
2023-07-19 19:39:14 -0600

    se anhadio la logica de conversion solo con metodos static

--- src/LogicsConversor.java
+++ src/LogicsConversor.java
@@ -1,27 +1,79 @@
 public class LogicsConversor {
-    //public int opt = 0;
+
     public static String convert(Object item, String amount){
 
-        try{
-            return LogicsConversor.pesotodollar(amount);
+        try {
+            int i = (int) item; //Convertir el "Objeto" a tipo int
+            String answer = "Ingrese un monto";
+            //double proportion = 0;
+            switch (i) {
+                case 0 -> {
+                    double proportion = 0.0598;
+                    answer = LogicsConversor.convertcurrency(amount, proportion) + " USD";
+                }
+                case 1 -> {
+                    double proportion = 0.0534;
+                    answer = LogicsConversor.convertcurrency(amount, proportion) + " EUR";
+                }
+                case 2 -> {
+                    double proportion = 0.0463;
+                    answer = LogicsConversor.convertcurrency(amount, proportion) + " GBP";
+                }
+                case 3 -> {
+                    double proportion = 8.3525;
+                    answer = LogicsConversor.convertcurrency(amount, proportion) + " JPY";
+                }
+                case 4 -> {
+                    double proportion = 75.74;
+                    answer = LogicsConversor.convertcurrency(amount, proportion) + " KRW";
+                }
+                //--------------------------------------------------------------------------
+                case 5 -> {
+                    double proportion = 16.712;
+                    answer = LogicsConversor.convertcurrency(amount, proportion) + " MXN";
+                }
+                case 6 -> {
+                    double proportion = 18.73;
+                    answer = LogicsConversor.convertcurrency(amount, proportion) + " MXN";
+                }
+                case 7 -> {
+                    double proportion = 21.6304;
+                    answer = LogicsConversor.convertcurrency(amount, proportion) + " MXN";
+                }
+                case 8 -> {
+                    double proportion = 0.1198;
+                    answer = LogicsConversor.convertcurrency(amount, proportion) + " MXN";
+                }
+                case 9 -> {
+                    double proportion = 0.0132;
+                    answer = LogicsConversor.convertcurrency(amount, proportion) + " MXN";
+                }
+
+                default -> {
+                    return answer;
+                }
+            }
+            return answer;
 
         }catch (Exception exception){
-            System.out.println("No se ingresó un monto");
-            return "Ingresa un número";
+            System.out.println("Excepción: No se ingresó un monto");
+            return "Ingresa un número válido";
         }
     }
 
-    public static String pesotodollar(String amount){
-        //System.out.println(item);
 
-        double peso = Double.parseDouble(amount); //Convertir String a double
 
-        System.out.println(peso);
+    public static String convertcurrency(String amount, double proportion){
 
-        double conversion = peso * 0.0597;
+        double currency = Double.parseDouble(amount); //Convertir String a double
+
+        System.out.println(currency);
+
+        double conversion = currency * proportion;
 
         System.out.println(conversion);
 
         return String.valueOf(conversion); //Convierte double a String para retornarlo
     }
+    
 }
